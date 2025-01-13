1520aa538edbff7e8598083bcac0e6b331e2f63e
Sankalp kumar <sankalpkumar911@gmail.com>
2025-01-13 22:33:40 +0530

    some change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["*"], // This scans all files in the project for Tailwind classes
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scrollDown: 'scrollDown 8s ease-in-out', // Add custom animation
+      },
+      keyframes: {
+        scrollDown: {
+          '0%': {
+            transform: 'translateY(0)', // Initial position (start)
+          },
+          '100%': {
+            transform: 'translateY(-75%)', // Move the image up by 75% (end)
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
